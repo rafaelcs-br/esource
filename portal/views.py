f29ed97ff664005a09eed641d505dce2fe18f1ec
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2026-04-25 21:33:58 -0300

    ✨ feat: Adicionando Cards

--- portal/views.py
+++ portal/views.py
@@ -1,32 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from portal.models import Artigo, Evento, Autor
+from portal.utils.context import get_context
 
 # Create your views here.
 def artigos(request):
     artigos = Artigo.objects.all().filter(status='DIS')
     
-    context = {
-        'artigos': artigos
-    }
+    context = get_context({'artigos': artigos})
     
     return render(request, 'artigos.html', context)
 
 def eventos(request):
     eventos = Evento.objects.all().filter(status='DIS')
     
-    context = {
-        'eventos': eventos
-    }
+    context = get_context({'eventos': eventos})
     
     return render(request, 'eventos.html', context)
 
 def autores(request):
     autores = Autor.objects.all()
     
-    context = {
-        'autores': autores
-    }
-    
+    context = get_context({'autores': autores})
+
     return render(request, 'autores.html', context)
 
 def detalhes_artigo(request, artigo_id):
